fab8f2bdde254ba4d3d1aa1130d2acd8a758b99e
washington panganai <washpangs@gmail.com>
2025-06-06 22:32:26 +0100

    new template

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Post
 
@@ -9,3 +12,14 @@ class HomePage(ListView):
     model = Post
     context_object_name ="posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
+
+class PostDetail(DetailView):
+    http_method_names = ["get"]
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post"
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model  = Post
+    template_name = "feed/create.html"
+    fields = ['text']
